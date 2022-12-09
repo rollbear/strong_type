e022ae9a92efa9650732afa0d804c5b7022040f7
Björn Fahller <bjorn@fahller.se>
2022-12-09 10:40:06 +0100

    Add tests for strong::unique

--- test.cpp
+++ test.cpp
@@ -806,6 +806,45 @@ TEST_CASE("equality_with can compare with defined type")
     REQUIRE(t2{2} != t1{1});
 }
 
+TEST_CASE("strong::unique is movable")
+{
+    using t = strong::type<std::unique_ptr<int>, struct t_, strong::unique>;
+
+    GIVEN("an initialized source")
+    {
+        t source{std::make_unique<int>(3)};
+        auto* addr = value_of(source).get();
+
+        WHEN("move constructing a dest")
+        {
+            auto dest = std::move(source);
+            THEN("the source is moved from")
+            {
+                REQUIRE(value_of(source).get() == nullptr);
+            }
+            AND_THEN("the dest has the value that source had")
+            {
+                REQUIRE(*value_of(dest) == 3);
+                REQUIRE(value_of(dest).get() == addr);
+            }
+        }
+        AND_WHEN("move assigning to another instance")
+        {
+            t dest{std::make_unique<int>(4)};
+            dest = std::move(source);
+            THEN("the source is moved from")
+            {
+                REQUIRE(value_of(source).get() == nullptr);
+            }
+            AND_THEN("the dest has the value that source had")
+            {
+                REQUIRE(*value_of(dest) == 3);
+                REQUIRE(value_of(dest).get() == addr);
+            }
+        }
+    }
+}
+
 TEST_CASE("an ostreamable type can be streamed using stream flags")
 {
   strong::type<int, struct i_, strong::ostreamable> i{3};
