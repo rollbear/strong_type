5fedd3113bc256f443f2294ff63d09524190cc41
Björn Fahller <bjorn@fahller.se>
2022-12-23 14:35:49 +0100

    Move strong::affine_point to a minimal header file, and seperate tests

--- include/strong_type/strong_type.hpp
+++ include/strong_type/strong_type.hpp
@@ -28,7 +28,7 @@
 #include "pointer.hpp"
 #include "hashable.hpp"
 #include "difference.hpp"
-
+#include "affine_point.hpp"
 
 #include <istream>
 #include <ostream>
@@ -68,105 +68,6 @@ namespace strong
 {
 
 
-template <typename D = void>
-struct affine_point
-{
-  template <typename T>
-  class modifier;
-};
-
-namespace impl
-{
-  template <typename ...>
-  using void_t = void;
-
-  template <typename T, typename = void>
-  struct subtractable : std::false_type {};
-
-  template <typename T>
-  struct subtractable<T, void_t<decltype(std::declval<const T&>() - std::declval<const T&>())>>
-  : std::true_type {};
-}
-
-
-template <typename D>
-template <typename T, typename Tag, typename ... M>
-class affine_point<D>::modifier<::strong::type<T, Tag, M...>>
-{
-  using type = ::strong::type<T, Tag, M...>;
-  static_assert(impl::subtractable<T>::value, "it must be possible to subtract instances of your underlying type");
-  using base_diff_type = decltype(std::declval<const T&>() - std::declval<const T&>());
-public:
-  using difference = std::conditional_t<std::is_same<D, void>{}, strong::type<base_diff_type, Tag, strong::difference>, D>;
-  static_assert(std::is_constructible<difference, base_diff_type>::value, "");
-  STRONG_NODISCARD
-  friend
-  STRONG_CONSTEXPR
-  difference
-  operator-(
-    const type& lh,
-    const type& rh)
-  {
-    return difference(value_of(lh) - value_of(rh));
-  }
-
-  friend
-  STRONG_CONSTEXPR
-  type&
-  operator+=(
-    type& lh,
-    const difference& d)
-  noexcept(noexcept(value_of(lh) += impl::access(d)))
-  {
-    value_of(lh) += impl::access(d);
-    return lh;
-  }
-
-  friend
-  STRONG_CONSTEXPR
-  type&
-  operator-=(
-    type& lh,
-    const difference& d)
-  noexcept(noexcept(value_of(lh) -= impl::access(d)))
-  {
-    value_of(lh) -= impl::access(d);
-    return lh;
-  }
-
-  STRONG_NODISCARD
-  friend
-  STRONG_CONSTEXPR
-  type
-  operator+(
-    type lh,
-    const difference& d)
-  {
-    return lh += d;
-  }
-
-  STRONG_NODISCARD
-  friend
-  STRONG_CONSTEXPR
-  type
-  operator+(
-    const difference& d,
-    type rh)
-  {
-    return rh+= d;
-  }
-
-  STRONG_NODISCARD
-  friend
-  STRONG_CONSTEXPR
-  type
-  operator-(
-    type lh,
-    const difference& d)
-  {
-    return lh -= d;
-  }
-};
 
 
 
--- test/test.cpp
+++ test/test.cpp
@@ -813,61 +813,6 @@ TEST_CASE("indexed can be accessed with operator[] without at()")
     REQUIRE(std::move(as_const(a))[2] == 5);
 }
 
-TEST_CASE("affine_point types can be subtracted")
-{
-  using D = strong::type<int, struct i_>;
-  using T = strong::type<int, struct i_, strong::affine_point<D>>;
-
-  T t1{3};
-  T t2{8};
-  auto d = t2 - t1;
-  static_assert(std::is_same<decltype(d), D>{}, "");
-  REQUIRE(value_of(d) == 5);
-}
-
-TEST_CASE("affine point type with defaulted difference type yields it when subtracting")
-{
-    using T = strong::type<int, struct i_, strong::affine_point<>>;
-
-    T t1{3};
-    T t2{8};
-    auto d = t2 - t1;
-    static_assert(std::is_same<decltype(d), strong::type<int, struct i_, strong::difference>>{},"");
-    REQUIRE(value_of(d) == 5);
-}
-
-TEST_CASE("affine_point types can be added with the delta type")
-{
-  using D = strong::type<int, struct i_>;
-  using T = strong::type<int, struct i_, strong::affine_point<D>>;
-
-  T t1{8};
-  D d{3};
-
-  auto t2 = t1 + d;
-  static_assert(std::is_same<decltype(t2), T>{}, "");
-  REQUIRE(value_of(t2) == 11);
-  auto t3 = d + t1;
-  static_assert(std::is_same<decltype(t3), T>{}, "");
-  REQUIRE(value_of(t3) == 11);
-  t1 += d;
-  REQUIRE(value_of(t1) == 11);
-}
-
-TEST_CASE("affine_point types can be subtracted with the delta type")
-{
-  using D = strong::type<int, struct i_>;
-  using T = strong::type<int, struct i_, strong::affine_point<D>>;
-
-  T t1{8};
-  D d{3};
-
-  auto t2 = t1 - d;
-  static_assert(std::is_same<decltype(t2), T>{}, "");
-  REQUIRE(value_of(t2) == 5);
-  t1 -= d;
-  REQUIRE(value_of(t1) == 5);
-}
 
 
 TEST_CASE("iterators work with algorithms")
