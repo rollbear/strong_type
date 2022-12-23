023cdc435a9dc71c8e937386c9b351b57377db17
Björn Fahller <bjorn@fahller.se>
2022-12-23 09:34:25 +0100

    A file that only includes stong_type.hpp builds clean

---
+++ test/empty.cpp
@@ -0,0 +1,16 @@
+/*
+ * strong_type C++14/17/20 strong typedef library
+ *
+ * Copyright (C) Björn Fahller
+ *
+ *  Use, modification and distribution is subject to the
+ *  Boost Software License, Version 1.0. (See accompanying
+ *  file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * Project home: https://github.com/rollbear/strong_type
+ */
+
+
+
+#include <strong_type/strong_type.hpp>
