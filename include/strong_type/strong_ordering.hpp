eaad8b000869b1decd3a79d7f6a74ef8a17dfce5
Björn Fahller <bjorn@fahller.se>
2023-05-02 18:59:25 +0200

    Add gcc-13 to CI matrix

---
+++ include/strong_type/strong_ordering.hpp
@@ -0,0 +1,31 @@
+#ifndef STRONG_TYPE_STRONG_ORDERING_HPP
+#define STRONG_TYPE_STRONG_ORDERING_HPP
+
+#include <compare>
+
+#include "type.hpp"
+
+namespace strong
+{
+struct strong_ordering
+{
+    template <typename T>
+    class modifier;
+};
+
+template <typename T, typename Tag, typename ... M>
+class strong_ordering::modifier<::strong::type<T, Tag, M...>>
+{
+    using type = ::strong::type<T, Tag, M...>;
+public:
+    STRONG_NODISCARD
+    friend
+    STRONG_CONSTEXPR
+    std::strong_ordering operator<=>(const type& lh, const type& rh) noexcept
+    {
+      return value_of(lh) <=> alue_of(rh);
+    }
+};
+}
+
+#endif //STRONG_TYPE_STRONG_ORDERING_HPP
